d4552862830508e832bf0d2ea5b1ab7395e5ab63
shravan1998 <shrkum51@gmail.com>
2019-02-20 23:48:54 +0530

    index file modified

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 const express = require("express");
-const app = app();
-const crawler =  require("crawler");
+const app = express();
+//const crawler =  require("");
+const bodyParser = require("body-parser");
+let insta_video = require("./routes/insta_video");
+let insta_image = require("./routes/insta_image");
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended:true
+}));
+app.use(function(req,res,next){
+    res.header("Access-Control-Allow-Origin","*");
+    res.header("Access-Control-Allow-Headers","Origin,X-Requested-With,Content-Type,Accept");
+});
+app.get('/',function(req,res){
+    res.send("Ready When You Are");
+});
+app.get('/instagram/video',insta_video);
+app.get('/instagram/image',insta_image);
 app.listen(8000,function(){
     console.log("Initialized");
 });
